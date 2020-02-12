039015567cadde965c105f0270f57d4e38c68ab2
Daisky <nakamura.daisky@gmail.com>
2020-02-13 01:30:07 +0900

    router-linkを名前付きの書き方にする

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/devcourse">DevCouse</router-link>
+      <router-link :to="{name: 'About' }">About</router-link> |
+      <router-link :to="{name: 'dev' }">DevCouse</router-link>
     </div>
     <router-view/>
   </div>
