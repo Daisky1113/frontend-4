4974c55936001ffef1a70685a87eb9a0ddee1209
Daisky <nakamura.daisky@gmail.com>
2020-02-13 01:12:23 +0900

    ViewsにTopPage.vueを作成してindex.htmlを移植

---
+++ src/views/TopPage.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="index">
+<div id="wrapper">
+<header><div class="inner">
+<h1 class="id"><a href="/"><img src="/data/images/header_title.png" width="233" height="74" alt="G's Academy Tokyo"></a></h1>
+<p class="txt"><img src="/data/images/header_txt.png" width="134" height="69" alt="Become a geek with the power to change the world."></p>
+</div></header>
+<nav id="gNav" class="gNav201605">
+<ul>
+
+<li class="nav02"><a href="/about/"><img src="/data/images/gNav_about.png" width="158" height="80" alt="About"></a></li>
+<li class="nav03"><a href="/devcourse/"><img src="/data/images/gNav_tokyodevcourse.png" width="158" height="80" alt="Tokyo DEV Course"></a></li>
+<li class="nav07"><a href="/labcourse/"><img src="/data/images/gNav_tokyolabcourse.png" width="158" height="80" alt="Tokyo LAB Course"></a></li>
+<li class="nav08"><a href="/fukuokadev/"><img src="/data/images/gNav_fukuokadevcourse.png" width="158" height="80" alt="Fukuoka DEV Course"></a></li>
+
+
+<li class="nav09"><a href="/fukuokalab/"><img src="/data/images/gNav_fukuokalabcourse.png" width="158" height="80" alt="Fukuoka LAB Course"></a></li>
+<li class="apprication nav06"><a href="https://krs.bz/dhw/m/gs_form" target="_blank"><img src="/data/images/gNav_nav06.png" width="156" height="76" alt="Entry"></a></li></ul>
+</nav>
+<div id="intro" class="block intro201605"><div class="inner">
+<p class="txt"><img src="/data/images/201605_index_intro_txt.png" width="480" height="68" alt="セカイを変えるGEEKになろう。エンジニア養成学校『ジーズアカデミーTOKYO』"></p>
+<p class="catch fff">春の全コースで<br class="sp">入学試験の受付中！<br>東京・福岡で<br class="sp">学校説明会を開催！</p>
+<p class="btn"><a href="https://krs.bz/dhw/m/gs_form" target="_blank">説明会日程はこちら</a></p>
+
+</div></div>
+</div>
+</div>
+</template>
