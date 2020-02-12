ad6b32d3b35b40631029f7218af336ccbed61c9e
Daisky <nakamura.daisky@gmail.com>
2020-02-13 01:44:52 +0900

    components配下にMyHeader.vueを作ってAbout.vueからheader部分を移植する

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,11 @@
+<template>
+<header><div class="inner">
+<p class="id"><a href="/"><img src="/data/images/header_title.png" width="233" height="74" alt="G's Academy Tokyo"></a></p>
+<p class="txt"><img src="/data/images/header_txt.png" width="134" height="69" alt="Become a geek with the power to change the world."></p>
+</div></header>
+</template>
+<script>
+  export default{
+    name: 'MyHeader',
+  }
+</script>
