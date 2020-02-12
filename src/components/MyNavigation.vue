675b025998390020af05a5518f16c45dcf273a0c
Daisky <nakamura.daisky@gmail.com>
2020-02-13 02:00:11 +0900

    MyNavigationコンポーネントを変更

--- src/components/MyNavigation.vue
+++ src/components/MyNavigation.vue
@@ -1,15 +1,10 @@
 <template>
   <nav id="gNav" class="gNav201605">
 <ul>
-
 <li class="nav02"><a href="/about/"><img src="/data/images/gNav_about.png" width="158" height="80" alt="About"></a></li>
 <li class="nav03"><a href="/devcourse/"><img src="/data/images/gNav_tokyodevcourse.png" width="158" height="80" alt="Tokyo DEV Course"></a></li>
 <li class="nav07"><a href="/labcourse/"><img src="/data/images/gNav_tokyolabcourse.png" width="158" height="80" alt="Tokyo LAB Course"></a></li>
-<li class="nav08"><a href="/fukuokadev/"><img src="/data/images/gNav_fukuokadevcourse.png" width="158" height="80" alt="Fukuoka DEV Course"></a></li>
-
-
-<li class="nav09"><a href="/fukuokalab/"><img src="/data/images/gNav_fukuokalabcourse.png" width="158" height="80" alt="Fukuoka LAB Course"></a></li>
-<li class="apprication nav06"><a href="https://krs.bz/dhw/m/gs_form" target="_blank"><img src="/data/images/gNav_nav06.png" width="156" height="76" alt="Entry"></a></li></ul>
+</ul>
 </nav>
 </template>
 <script>
