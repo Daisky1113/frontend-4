2b578f4f70839346bbe385f24162d033b046568d
Daisky <nakamura.daisky@gmail.com>
2020-02-13 04:37:21 +0900

    vuetifyからappbar をとってきてコンポーネントにうつす。App.vueでコンポーネントを読み込む

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <v-app>
+    <MyHeader/>
   </v-app>
 </template>
 
 <script>
-
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
 
   components: {
-    
+    MyHeader
   },
 
   data: () => ({
