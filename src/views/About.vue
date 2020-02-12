ba9bd60d563867e06c0486b10cfb54c07d375836
Daisky <nakamura.daisky@gmail.com>
2020-02-13 01:32:03 +0900

    元々あったAbout.vueの中身を書き換える

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div id="wrapper">
+<header><div class="inner">
+<p class="id"><a href="/"><img src="/data/images/header_title.png" width="233" height="74" alt="G's Academy Tokyo"></a></p>
+<p class="txt"><img src="/data/images/header_txt.png" width="134" height="69" alt="Become a geek with the power to change the world."></p>
+</div></header>
+<nav id="gNav" class="gNav201605">
+<ul>
+
+<li class="nav02"><a href="/about/"><img src="/data/images/gNav_about.png" width="158" height="80" alt="About"></a></li>
+<li class="nav03"><a href="/devcourse/"><img src="/data/images/gNav_tokyodevcourse.png" width="158" height="80" alt="Tokyo DEV Course"></a></li>
+<li class="nav07"><a href="/labcourse/"><img src="/data/images/gNav_tokyolabcourse.png" width="158" height="80" alt="Tokyo LAB Course"></a></li>
+<li class="nav08"><a href="/fukuokadev/"><img src="/data/images/gNav_fukuokadevcourse.png" width="158" height="80" alt="Fukuoka DEV Course"></a></li>
+
+
+<li class="nav09"><a href="/fukuokalab/"><img src="/data/images/gNav_fukuokalabcourse.png" width="158" height="80" alt="Fukuoka LAB Course"></a></li>
+<li class="apprication nav06"><a href="https://krs.bz/dhw/m/gs_form" target="_blank"><img src="/data/images/gNav_nav06.png" width="156" height="76" alt="Entry"></a></li></ul>
+</nav>
+<main id="contents">
+<div id="intro" class="block"><div class="inner">
+<div class="body"><h1 class="title"><span><img src="/data/images/about_title.png" width="86" height="18" alt="About"></span><span>G's ACADEMYとは</span></h1></div>
+</div></div>
+
+<section id="concept" class="block"><div class="inner">
+<h2 class="title"><span><img src="/data/images/index_concept_title.png" width="120" height="18" alt="Concept"></span><span>コンセプト</span></h2>
+<h3>セカイを変えるGEEKになろう。</h3>
+<p>今、世界中で新たなサービスやエンタテインメントがエンジニアによって生み出されています。<br>その武器は<strong>『アイディア』</strong>と<strong>『プログラミング<span class="krnR">』</span></strong>。</p>
+<p>ワクワクするようなゲームのアイディア<br>世の中が笑顔になるようなサービスのアイディア<br>儲かりそうな新規事業のアイディア<br>そんなものをため込んでいるあなたこそ、<br>プロレベルのプログラミングスキルをもつべきだと思うのです。</p>
+<p>できるだけ多くの若い人に本格的なプログラミングスキルを学ぶ機会をつくりたい。<br>そして願わくば、この場所から世界中の人が利用するサービスやアプリが産まれてほしい。</p>
+<p>そんな思いでデジタルハリウッドが<br>エンジニアのためだけの場所<strong>「ジーズアカデミーTOKYO」</strong>をつくりました。</p>
+<p>最初は全くの初心者でOK。<br>まずは純粋にプログラミングを楽しんでいただくことから始めて、<br>最後には現役で活躍する一流エンジニアのメンターサポートをうけながら、<br>オリジナルのWebサービス・アプリを開発〜完成させます。</p>
+<p>卒業後の<strong>「就職」</strong>はもちろん<strong>「独立」</strong>まで、さまざまなサポート企業や<br>シードアクセラレーターがバックアップ。</p>
+<p>さあ、まもなく<strong>【GEEK】</strong>への扉がひらかれます。</p>
+</div>
+</section>
+</main>
+</div>
 </template>
