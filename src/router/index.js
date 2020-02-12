e4fee16e66e1be3b421260cc170458171645b260
Daisky <nakamura.daisky@gmail.com>
2020-02-13 08:01:12 +0900

    viewsにtodo.vueを作ってrouteに追加

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Todo from '../views/Todo.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -22,6 +23,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
   }
 ]
 
---
+++ src/views/Todo.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <TodoTable
+          :todoDatas = "todos"
+          todoTitle = "Todo"
+        />
+      </v-col>
+      <v-col cols="12" md="6">
+        <TodoDoneTable
+          :doneDatas = "done"
+          todoTitle = "Done"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import TodoTable from '../components/TodoTable.vue'
+import TodoDoneTable from '../components/TodoDoneTable.vue'
+export default{
+  components: {
+    TodoTable,
+    TodoDoneTable
+  },
+  data: function(){
+    return {
+      todos: [
+        {
+          name: 'lorem ipsum lorem ipsum',
+          id: 1
+        },
+        {
+          name: 'test2',
+          id: 2
+        },
+        {
+          name: 'test3',
+          id: 3
+        }
+      ],
+      done: [
+        {
+          name: 'foo',
+          date: '2020-02-20'
+        },
+        {
+          name: 'foobarhoge',
+          date: '2020-02-21'
+        }        
+      ]
+    }
+  }
+}
+</script>
