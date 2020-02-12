9227df266857ca0f96a22a6d764e412efc54fdf1
Daisky <nakamura.daisky@gmail.com>
2020-02-13 02:04:57 +0900

    MyNavigationコンポーネントのリンクをrouter-linkに変更する

--- src/components/MyNavigation.vue
+++ src/components/MyNavigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav id="gNav" class="gNav201605">
 <ul>
-<li class="nav02"><a href="/about/"><img src="/data/images/gNav_about.png" width="158" height="80" alt="About"></a></li>
-<li class="nav03"><a href="/devcourse/"><img src="/data/images/gNav_tokyodevcourse.png" width="158" height="80" alt="Tokyo DEV Course"></a></li>
-<li class="nav07"><a href="/labcourse/"><img src="/data/images/gNav_tokyolabcourse.png" width="158" height="80" alt="Tokyo LAB Course"></a></li>
+<li class="nav02"><router-link :to="{name: 'About' }"><img src="/data/images/gNav_about.png" width="158" height="80" alt="About"></router-link>
+<li class="nav03"><router-link :to="{name: 'dev' }"><img src="/data/images/gNav_tokyodevcourse.png" width="158" height="80" alt="Tokyo DEV Course"></router-link></li>
+<li class="nav07"><router-link :to="{name: 'lab' }"><img src="/data/images/gNav_tokyolabcourse.png" width="158" height="80" alt="Tokyo LAB Course"></router-link></li>
 </ul>
 </nav>
 </template>
