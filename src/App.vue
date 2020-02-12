c6d88f37fb2dfb6b9477cdd96a1ab659ad818d64
Daisky <nakamura.daisky@gmail.com>
2020-02-13 06:01:10 +0900

    Vuetifyのnavigation drawerからSideNavコンポーネントを作り、App.vueで読み込む

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     <MyHeader
       pageTitle = "Hello Vuetify"
     />
+    <SideNav/>
   </v-app>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue'
-
+import SideNav from './components/SideNav.vue'
 export default {
   name: 'App',
 
   components: {
-    MyHeader
+    MyHeader,
+    SideNav
   },
 
   data: () => ({
