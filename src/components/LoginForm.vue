80cea3ac7d49688dc2e34700ccd4cabe3e126cbc
Daisky <nakamura.daisky@gmail.com>
2020-02-13 07:05:16 +0900

    loginForm.vueをemailとパスワードを入力できる形にする

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,24 +3,62 @@
     class="mx-auto"
     max-width="344"
   >
-    <v-card-text>
-      <div>Word of the Day</div>
-      <p class="display-1 text--primary">
-        be•nev•o•lent
-      </p>
-      <p>adjective</p>
-      <div class="text--primary">
-        well meaning and kindly.<br>
-        "a benevolent smile"
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
+    <v-app-bar
         color="deep-purple accent-4"
-      >
-        Learn More
-      </v-btn>
-    </v-card-actions>
+        dense
+        dark
+        flat
+    >
+      <v-toolbar-title>Login</v-toolbar-title>
+     
+    </v-app-bar>
+      <v-progress-linear
+        indeterminate
+        color="pink"
+      ></v-progress-linear> 
+    <v-card-text>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+          <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.emailMatch]"
+            label="email"
+          ></v-text-field>            
+          <v-text-field
+              v-model="password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              label="password"
+              @click:append="show1 = !show1"
+            ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-btn small>login</v-btn>
+        </v-container>
+      </v-form>
+    </v-card-text>
   </v-card>
 </template>
+<script>
+  export default {
+data () {
+      return {
+        show1: false,
+        password: '',
+        email: '',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => ('The email and password you entered don\'t match'),
+        },
+      }
+    },
+  }
+</script>
