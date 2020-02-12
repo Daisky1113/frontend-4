bde203503fec4693b94e12cf3b127dfea93614c9
Daisky <nakamura.daisky@gmail.com>
2020-02-13 01:50:35 +0900

    DevCourse.vueとLabCourse.vueもMyheaderコンポーネントをインポートする

--- src/views/LabCourse.vue
+++ src/views/LabCourse.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="labcourse">
 <div id="wrapper">
-<header><div class="inner">
-<p class="id"><a href="/"><img src="/data/images/header_title.png" width="233" height="74" alt="G's Academy Tokyo"></a></p>
-<p class="txt"><img src="/data/images/header_txt.png" width="134" height="69" alt="Become a geek with the power to change the world."></p>
-</div></header>
+<MyHeader/>
 <nav id="gNav" class="gNav201605">
 <ul>
 
@@ -33,3 +30,12 @@
 </div>
 </div>
 </template>
+<script>
+  import MyHeader from '../components/MyHeader.vue'
+  export default{
+    name: 'LabCourse',
+    components: {
+      MyHeader
+    }
+  }
+</script>
